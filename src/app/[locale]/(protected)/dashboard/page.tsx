af4c7a391a570d98e884ebd260b02f18dc6d0098
Alexandre Dissi <alexandredissi@icloud.com>
2025-03-21 00:16:37 +0100

    rebase

--- src/app/[locale]/(protected)/dashboard/page.tsx
+++ src/app/[locale]/(protected)/dashboard/page.tsx
@@ -3,5 +3,7 @@ import CalendarWidget from "@/components/calendar/calendarWidget";
 
 export default async function Home() {
   const token = (await cookies()).get("token")?.value;
+  console.log(token);
+
   return <CalendarWidget bearer={token} />;
 }
